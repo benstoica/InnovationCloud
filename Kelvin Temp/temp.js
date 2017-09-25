3b170b2d7fa75699f2ea4a06d700d3030d13fef9
Ben Stoica <bennystoica@gmail.com>
2017-09-24 21:59:29 -0400

    Delete temp.js

--- Kelvin Temp/temp.js
+++
@@ -1,9 +0,0 @@
-//sets temp in kelvin//
-const kelvin = prompt('What is the Kelvin temperature today');
-//calculates temp in celsius//
-let celsius = kelvin - 273;
-//calculates temp in fahrenheit//
-let fahrenheit = Math.floor(celsius * (9/5) + 32);
-
-console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
-
